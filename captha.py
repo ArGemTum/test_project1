c3f7ad2c00b9d3cbc4641da7d91343cdd35dd2a8
ArGemTum <144426285+ArGemTum@users.noreply.github.com>
2024-06-26 19:59:01 +0300

    Add files via upload

---
+++ captha.py
@@ -0,0 +1,104 @@
+import sys
+import random
+import string
+import io
+from PIL import Image, ImageDraw, ImageFont
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QPixmap, QImage, QFont
+
+
+class CaptchaApp(QWidget):
+    def __init__(self, main_app_callback):
+        """
+        Инициализирует виджет CAPTCHA и устанавливает функцию обратного вызова для основного приложения.
+        """
+        super().__init__()
+        self.main_app_callback = main_app_callback
+        self.initUI()
+
+    def initUI(self):
+        """
+        Инициализирует пользовательский интерфейс для CAPTCHA и авторизации.
+        """
+        self.setWindowTitle('Авторизация')
+
+        font = QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(11)
+        self.login_label = QLabel('Логин', self)
+        self.login_entry = QLineEdit(self)
+        self.login_label.setFont(font)
+
+        self.password_label = QLabel('Пароль', self)
+        self.password_label.setFont(font)
+        self.password_entry = QLineEdit(self)
+        self.password_entry.setEchoMode(QLineEdit.Password)
+
+        self.captcha_label = QLabel('Введите каптчу', self)
+        self.captcha_label.setFont(font)
+        self.captcha_entry = QLineEdit(self)
+
+        self.captcha_image_label = QLabel(self)
+
+        self.refresh_button = QPushButton('Обновить каптчу', self)
+        self.refresh_button.setFont(font)
+        self.refresh_button.clicked.connect(self.generate_captcha)
+
+        self.submit_button = QPushButton('Войти', self)
+        self.submit_button.setFont(font)
+        self.submit_button.clicked.connect(self.validate)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.login_label)
+        layout.addWidget(self.login_entry)
+        layout.addWidget(self.password_label)
+        layout.addWidget(self.password_entry)
+        layout.addWidget(self.captcha_label)
+        layout.addWidget(self.captcha_entry)
+        layout.addWidget(self.captcha_image_label)
+        layout.addWidget(self.refresh_button)
+        layout.addWidget(self.submit_button)
+
+        self.setLayout(layout)
+        self.generate_captcha()
+
+    def generate_captcha(self):
+        """
+        Генерирует случайный CAPTCHA текст, создает изображение CAPTCHA и отображает его в интерфейсе.
+        """
+        self.captcha_text = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+        image = Image.new('RGB', (210, 50), (255, 255, 255))
+        font = ImageFont.truetype("arial.ttf", 36)
+        draw = ImageDraw.Draw(image)
+        draw.text((10, 5), self.captcha_text, font=font, fill=(0, 0, 0))
+
+        byte_arr = io.BytesIO()
+        image.save(byte_arr, format='PNG')
+        byte_arr.seek(0)
+        qimage = QImage()
+        qimage.loadFromData(byte_arr.read())
+
+        self.captcha_image = QPixmap.fromImage(qimage)
+        self.captcha_image_label.setPixmap(self.captcha_image)
+
+    def validate(self):
+        """
+        Проверяет правильность введенных пользователем данных (логин, пароль и CAPTCHA).
+        Если данные корректны, скрывает виджет CAPTCHA и запускает основное приложение.
+        Если данные некорректны, показывает сообщение об ошибке.
+
+        Логин и пароль для проверки установлены жестко (login: "123", password: "123").
+        """
+        login = self.login_entry.text()
+        password = self.password_entry.text()
+        captcha_input = self.captcha_entry.text()
+
+        if login != "123" or password != "123":
+            QMessageBox.critical(self, 'Error', 'Please enter the correct login and password.')
+        elif captcha_input != self.captcha_text:
+            QMessageBox.critical(self, 'Error', 'Captcha does not match.')
+            self.generate_captcha()
+        else:
+            QMessageBox.information(self, 'Success', 'Login successful!')
+            self.hide()
+            self.main_app_callback()
---
+++ main.py
@@ -0,0 +1,118 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from MainMenu import Ui_MainMenu
+from DataBaseView import Ui_DataBaseView
+from captha import CaptchaApp
+import psycopg2
+
+def close():
+    """
+    Выход из приложения.
+    """
+    sys.exit()
+
+def back():
+    """
+    Возвращает к главному меню и закрывает окна просмотра базы данных.
+    """
+    MainMenu.show()
+    DataBaseView.close()
+
+def connect_to_db(ui_dbview):
+    """
+    Подключает к базе данных PostgreSQL и отображает данных выбранной таблицы.
+
+    Действия:
+    - Устанавливает параметры подключения к PostgreSQL (hostname, database, username, password, port).
+    - Получает имя выбранной таблицы из выпадающего списка.
+    - Пытается установить соединение с базой данных и выполнить SQL-запрос для получения данных из выбранной таблицы.
+    - Отображает полученные данные в виде таблицы в интерфейсе окна просмотра базы данных.
+    - В случае ошибки выводит сообщение об ошибке с деталями.
+
+    Замечания:
+    - Для подключения к базе данных используется модуль psycopg2.
+    """
+    hostname = 'localhost'
+    database = 'postgres'
+    username = 'postgres'
+    password = 'argemtum'
+    port = 5432
+    selected_table = ui_dbview.choose_db_table.currentText()
+
+    try:
+        connection = psycopg2.connect(
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=password,
+            port=port
+        )
+        QtWidgets.QMessageBox.information(None, "Успех", "Соединение с PostgreSQL успешно установлено")
+
+        cursor = connection.cursor()
+        query = f'SELECT * FROM fishing."{selected_table}"'
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+
+        model = QStandardItemModel()
+        model.setHorizontalHeaderLabels(columns)
+
+        for row in rows:
+            items = [QStandardItem(str(field)) for field in row]
+            model.appendRow(items)
+
+        ui_dbview.db_viewer.setModel(model)
+
+        cursor.close()
+        connection.close()
+
+    except Exception as error:
+        QtWidgets.QMessageBox.critical(None, "Ошибка", f"Ошибка подключения к PostgreSQL: {error}")
+
+def go_to_view_db():
+    """
+    Отображает окно просмотра базы данных путем переключения на него из главного меню.
+
+    Действия:
+    - Подключает обработчики событий для кнопок возврата к главному меню и подключения к базе данных.
+    - Заполняет выпадающий список таблицами базы данных для выбора пользователем.
+    """
+    global DataBaseView
+    DataBaseView = QtWidgets.QWidget()
+    DataBaseView.setFixedSize(1400, 800)
+    ui_dbview = Ui_DataBaseView()
+    ui_dbview.setupUi(DataBaseView)
+    DataBaseView.show()
+    MainMenu.close()
+    ui_dbview.go_back.clicked.connect(back)
+    ui_dbview.try_to_connect_db.clicked.connect(lambda: connect_to_db(ui_dbview))
+
+    tables = ["User", "Order", "Product", "OrderProduct", "Adress", "Role"]
+    ui_dbview.choose_db_table.addItems(tables)
+
+def show_main_app():
+    """
+    Отображает основное окно приложения.
+
+    Действия:
+    - Устанавливает обработчики событий для кнопок выхода из приложения и перехода к просмотру базы данных.
+    """
+    global MainMenu
+    MainMenu = QtWidgets.QMainWindow()
+    MainMenu.setFixedSize(1400, 800)
+    ui_main = Ui_MainMenu()
+    ui_main.setupUi(MainMenu)
+    MainMenu.show()
+
+    ui_main.exit_application.clicked.connect(close)
+    ui_main.go_to_database.clicked.connect(go_to_view_db)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    captcha = CaptchaApp(show_main_app)
+    captcha.setFixedSize(230, 300)
+    captcha.show()
+    sys.exit(app.exec_())
